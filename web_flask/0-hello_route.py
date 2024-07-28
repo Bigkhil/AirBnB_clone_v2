27c1da2c16d29d5fab75fba0a3aadeb0a76bf0ad
Mohamed khalil <khm40005000@outlook.com>
2024-07-28 21:30:49 +0300

    some changes

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+'''this script initiates flask'''
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def home():
+    '''this function handles home route'''
     return "Hello HBNB!"
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
