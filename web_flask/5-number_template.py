6a99ebfa9fcf5231eccec839105851615a809eaf
Mohamed khalil <khm40005000@outlook.com>
2024-07-29 16:57:53 +0300

    some changes

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+'''this script will start a flask web apllication'''
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    '''this function handles the home route'''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    '''this function handles the hbnb route'''
+    return 'HBNB'
+
+
+@app.route('/c/<string:text>', strict_slashes=False)
+def ctext(text):
+    '''this function handles the ctect route'''
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route('/python', strict_slashes=False, defaults={'text': 'is cool'})
+@app.route('/python/<text>', strict_slashes=False)
+def pthon_text(text):
+    '''this function handles the python route'''
+    return f"Python {text.replace('_', ' ')}"
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_n(n):
+    '''this function handles the number route'''
+    return f'{n} is a number'
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    '''this function handles the number_template route'''
+    return render_template('5-number.html', n=n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
